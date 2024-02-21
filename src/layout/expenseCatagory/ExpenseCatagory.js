875467f24021fc6fafe180619bf007c408e491d3
Aaliyan <aaliyanasif@outlook.com>
2024-02-21 17:54:21 +0500

    Add Expense Cat & Add Expense Catagory

---
+++ src/layout/expenseCatagory/ExpenseCatagory.js
@@ -0,0 +1,246 @@
+import React, { useEffect, useState } from "react";
+import { IconButton } from "@mui/material";
+import axios from "axios";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+
+// import "./styles.scss";
+import DataTable from "../../components/dataTable/DataTable";
+import Sidebar from "../../components/sidebar/SideBar";
+import Navbar from "../../components/navbar/Navbar";
+import { expenseCatagoryColumns } from "../../dataTableColumns";
+
+import { EXPENSE_CATAGORY_BASE_URL } from "../../utils/config";
+import ListHeader from "../../components/listHeader/ListHeader";
+import SnackBar from "../../components/alert/SnackBar";
+import Popup from "../../components/popup/Popup";
+
+export default function ExpenseCatagoryList() {
+  const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [severity, setSeverity] = useState("");
+  const [openPopup, setOpenPopup] = useState(false);
+  const [name, setName] = useState("");
+  // const [amount, setAmount] = useState("");
+  // const [description, setDescription] = useState("");
+  const [id, setId] = useState("");
+
+  var expense = {
+    name: "",
+  };
+  useEffect(() => {
+    getExpensesList();
+  }, []);
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 250,
+      renderCell: (params) => {
+        return (
+          <div className="cell-action">
+            <IconButton
+              aria-label="edit"
+              size="medium"
+              onClick={() => editExpense(params.row)}
+            >
+              <EditIcon fontSize="inherit" />
+            </IconButton>
+            <IconButton
+              aria-label="delete"
+              size="medium"
+              onClick={() => deleteExpense(params.row._id)}
+            >
+              <DeleteIcon fontSize="inherit" />
+            </IconButton>
+          </div>
+        );
+      },
+    },
+  ];
+  const getExpensesList = () => {
+    axios
+      .get(EXPENSE_CATAGORY_BASE_URL)
+      .then(function (response) {
+        if (response.data.error) {
+          setOpen(true);
+          setMessage(response.data.error_msg);
+          setSeverity("error");
+        } else {
+          setData(response.data.data);
+        }
+      })
+      .catch(function (error) {
+        setOpen(true);
+        setMessage("error: " + error);
+        setSeverity("error");
+      });
+  };
+  const deleteExpense = (id) => {
+    axios
+      .delete(EXPENSE_CATAGORY_BASE_URL + id)
+      .then(function (response) {
+        if (response.data.error) {
+          setOpen(true);
+          setMessage(response.data.error_msg);
+          setSeverity("error");
+        } else {
+          setOpen(true);
+          setMessage(response.data.success_msg);
+          setSeverity("success");
+        }
+      })
+      .catch(function (error) {
+        setOpen(true);
+        setMessage(error);
+        setSeverity("error");
+      });
+  };
+  const updateExpense = () => {
+    expense = {
+      name: name,
+      // amount: amount,
+      // description: description,
+    };
+    axios
+      .put(EXPENSE_CATAGORY_BASE_URL + id, expense)
+      .then(function (response) {
+        if (response.data.error) {
+          setOpen(true);
+          setMessage(response.data.error_msg);
+          setSeverity("error");
+        } else {
+          setOpen(true);
+          setMessage(response.data.success_msg);
+          setSeverity("success");
+          setName("");
+          setId("");
+          setOpenPopup(false);
+        }
+      })
+      .catch(function (error) {
+        setOpen(true);
+        setMessage("error: " + error);
+        setSeverity("error");
+      });
+  };
+  const validation = () => {
+    if (name.length === 0) {
+      setOpen(true);
+      setMessage("Some fields are missing");
+      setSeverity("error");
+    } else {
+      updateExpense();
+    }
+  };
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+  const editExpense = (expense) => {
+    setOpenPopup(true);
+    setName(expense.name);
+    // setDescription(expense.description);
+    // setAmount(expense.amount);
+    setId(expense._id);
+  };
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="list-container">
+        <Navbar />
+        <ListHeader
+          header={"Expenses Catagory List"}
+          firstButton={true}
+          firstButtonText={"Add Expense"}
+        />
+        <DataTable
+          data={data}
+          columns={expenseCatagoryColumns.concat(actionColumn)}
+          isForTransaction={false}
+        />
+        <Popup
+          title="Expense Form"
+          openPopup={openPopup}
+          setOpenPopup={setOpenPopup}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="name"
+                name="name"
+                label="Name"
+                fullWidth
+                variant="outlined"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+            </Grid>
+            {/* <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="amount"
+                name="amount"
+                label="Amount"
+                fullWidth
+                variant="outlined"
+                value={amount}
+                onChange={(event) => setAmount(event.target.value)}
+              />
+            </Grid> */}
+            {/* <Grid item xs={12} sm={12}>
+              <TextField
+                id="description"
+                name="description"
+                label="Description"
+                fullWidth
+                variant="outlined"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+              />
+            </Grid> */}
+            {/* <Grid item xs={12} sm={6}></Grid> */}
+            <Grid item xs={12} sm={6}>
+              <Grid
+                justifyContent={"flex-end"}
+                container
+                spacing={1}
+                direction={"row"}
+              >
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    size="medium"
+                    color="success"
+                    onClick={() => validation()}
+                  >
+                    Update
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" size="medium" color="error">
+                    Cancel
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Popup>
+        <SnackBar
+          open={open}
+          severity={severity}
+          message={message}
+          handleClose={handleClose}
+        />
+      </div>
+    </div>
+  );
+}
